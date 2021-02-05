8a0b3336b67fb34a7a88e9fc6845adc9b9723d22
Booseel <40848968+Booseel@users.noreply.github.com>
2021-02-06 04:33:31 +0500

    add content in modal in price section

--- src/common/modal.tsx
+++ src/common/modal.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Modal = ({active, setActive, children}) => {
+const Modal = ({active, setActive, modalHeader, modalPrice, modalId, children}) => {
 
     return (
-        <div className={active ? "wrapper_modal active" : "wrapper_modal"} onClick={() => setActive(false)}>            
+        <div id={modalId} className={active ? "wrapper_modal active" : "wrapper_modal"} onClick={() => setActive(false)}>            
             <div className="modal" onClick={e => e.stopPropagation()}>
                 <div className="close_modal" onClick={() => setActive(false)}>закрыть</div>
+                <h3 className="modal_header">{modalHeader}</h3>
+                <div className="modal_price">
+                    <h5>{modalPrice}</h5>
+                </div>
+                <p className="modal_info">Введите данные для регистрации личного кабинета на платформе обучения</p>
                 {children}
             </div>
         </div>
--- src/components/price.js
+++ src/components/price.js
@@ -1,135 +1,163 @@
 import React, { useState } from 'react'
-import Link from 'next/link'
 
-import Button from "../common/button"
 import Card from "../common/card"
 import Modal from '../common/modal'
-import SignUp from '../common/form'
+import SignUp from '../common/signUp'
 
-const Price = (props) => {
+const cardNames = {
+    basic: "Базовая",
+    advanced: "Продвинутая",
+    full: "Полная",
+};
+
+const currentPrices = {
+    basic: 5000,
+    advanced: 10000,
+    full: 15000,
+}
+
+const cardKeys = {
+    basic: "basic",
+    advanced: "advanced",
+    full: "full",
+}
+
+const Price = () => {
+
+    const [cardNameModal, setCardNameModal] = useState('');
+    const [cardPriceModal, setCardPriceModal] = useState('');
+    const [cardKeyModal, setCardKeyModal] = useState('');
+
+    const openModal = (cardKeyModal, cardNameModal, cardPriceModal) => {
+        setModalActive(true)
+        cardNameModal = setCardNameModal(cardNameModal);
+        cardPriceModal = setCardPriceModal(cardPriceModal);
+        cardKeyModal = setCardKeyModal(cardKeyModal);
+        return (cardKeyModal, cardNameModal, cardPriceModal);
+    };
+    
     const [ modalActive, setModalActive ] = useState(false)
+    const basicPrice = 5000;
+    const advancedPrice = 10000;
+    const fullPrice = 15000;
+
     return (
-    <section id="price">
-        <Modal active={modalActive} setActive={setModalActive}>
-            <h3 className="modal_header">Базовая</h3>
-            <div className="modal_price">
-                <h5>5 000р</h5>
+        <section id="price">
+            <Modal modalId={cardKeyModal} active={modalActive} setActive={setModalActive} modalHeader={cardNameModal} modalPrice={cardPriceModal}>
+                <SignUp onClickRequirements={() => setModalActive(false)}/>
+            </Modal>
+            <h4 className="section_header">Выбери учебную программу</h4>
+            <div className="cards flex spc_arnd">            
+                <Card cardId={cardKeys.basic} variant="basic" cardName={cardNames.basic} cardDescription="1 неделя" lowerPrice="5 000" currentPrice={currentPrices.basic} higherPrice="7 500">
+                    <ul className="about_cource__list flex column cardlist">
+                        <li className="about_cource__list_item">
+                            <span>Загрузка и установка программы, знакомство с интерфейсом</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Ретушь и стандартная пластика</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Пресет и текстура</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Рисунок на лице</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Макияж</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Работа со слоями</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Материалы</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Освещение и отражение</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Публикация маски</span>
+                        </li>
+                    </ul>
+                    <div className="button primary_button" onClick={() => openModal(cardKeys.basic, cardNames.basic, basicPrice)}>КУПИТЬ</div>
+                </Card>
+                <Card cardId={cardKeys.advanced} variant="basic" cardName={cardNames.advanced} cardDescription="2 недели" lowerPrice="10 000" currentPrice={currentPrices.advanced} higherPrice="16 500" buttonVariant="one">
+                    <ul className="about_cource__list flex column cardlist">
+                        <li className="about_cource__list_item">
+                            <span>Замена фона</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Работа с текстурой камеры</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Цвет глаз и волос</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Переключение по тапу и добавление инструкций</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Технология частиц</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Реакция на эмоции</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Добавление объектов вокург головы</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Покадровая анимация и рандомный выбор</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Авторская покадровая анимация</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Эффект Kira-Kira</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Работа со слоями ретушь + пластика + пресет</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Отбеливание зубов</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Смазанное движение</span>
+                        </li>
+                    </ul>
+                    <div className="button primary_button" onClick={() => openModal(cardKeys.advanced, cardNames.advanced, advancedPrice)}>КУПИТЬ</div>
+                </Card>
+                <Card cardId={cardKeys.full} variant="basic" cardName={cardNames.full} cardDescription="4 недели" lowerPrice="15 000" currentPrice={currentPrices.full} higherPrice="22 500" buttonVariant="one">
+                <ul className="about_cource__list flex column cardlist">
+                        <li className="about_cource__list_item card_full">
+                            <span>Базовая + Продвинутая</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Добавление музыки и изменение голоса</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>3D объекты на лице и голове</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>3D объекты на шее и за спиной</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Кнопки выбора</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>3D анимация</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Авторская пластика</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Физика объектов</span>
+                        </li>
+                        <li className="about_cource__list_item">
+                            <span>Игры</span>
+                        </li>
+                    </ul>
+                    <div className="button primary_button" onClick={() => openModal(cardKeys.full, cardNames.full, fullPrice)}>КУПИТЬ</div>
+                </Card>
             </div>
-            <p className="modal_info">Введите данные для регистрации личного кабинета на платформе обучения</p>
-            <SignUp onClickRequirements={setModalActive}/>
-        </Modal>
-        <h4 className="section_header">Выбери учебную программу</h4>
-        <div className="cards flex spc_arnd">            
-            <Card cardId="basic" variant="basic" cardName="Базовая" cardDescription="1 неделя" lowerPrice="5 000" currentPrice="5 000" higherPrice="7 500">
-                <ul className="about_cource__list flex column cardlist">
-                    <li className="about_cource__list_item">
-                        <span>Загрузка и установка программы, знакомство с интерфейсом</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Ретушь и стандартная пластика</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Пресет и текстура</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Рисунок на лице</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Макияж</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Работа со слоями</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Материалы</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Освещение и отражение</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Публикация маски</span>
-                    </li>
-                </ul>
-                <div className="button primary_button" onClick={() => setModalActive(true)}>КУПИТЬ</div>
-            </Card>
-            <Card cardId="advanced" variant="basic" cardName="Продвинутая" cardDescription="2 недели" lowerPrice="10 000" currentPrice="10 000" higherPrice="16 500" buttonVariant="one">
-                <ul className="about_cource__list flex column cardlist">
-                    <li className="about_cource__list_item">
-                        <span>Замена фона</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Работа с текстурой камеры</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Цвет глаз и волос</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Переключение по тапу и добавление инструкций</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Технология частиц</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Реакция на эмоции</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Добавление объектов вокург головы</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Покадровая анимация и рандомный выбор</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Авторская покадровая анимация</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Эффект Kira-Kira</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Работа со слоями ретушь + пластика + пресет</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Отбеливание зубов</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Смазанное движение</span>
-                    </li>
-                </ul>
-                <Button link="/" classes="button primary_button" linkText="КУПИТЬ" />
-            </Card>
-            <Card cardId="full" variant="basic" cardName="Полная" cardDescription="4 недели" lowerPrice="15 000" currentPrice="15 000" higherPrice="22 500" buttonVariant="one">
-               <ul className="about_cource__list flex column cardlist">
-                    <li className="about_cource__list_item card_full">
-                        <span>Базовая + Продвинутая</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Добавление музыки и изменение голоса</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>3D объекты на лице и голове</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>3D объекты на шее и за спиной</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Кнопки выбора</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>3D анимация</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Авторская пластика</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Физика объектов</span>
-                    </li>
-                    <li className="about_cource__list_item">
-                        <span>Игры</span>
-                    </li>
-                </ul>
-                <Button link="/" classes="button primary_button" linkText="КУПИТЬ" />
-            </Card>
-        </div>
-    </section>
+        </section>
     );
 }
 
